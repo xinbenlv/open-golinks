bbd105e6423f7cb4ef6eeb32486b403ce3ccc751
Zainan Victor Zhou <zzn@google.com>
2018-08-28 15:06:41 -0700

    Add typescript and ts-node for heroku deployment

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import * as express from "express";
 import * as http from "http";
 import * as bodyParser from "body-parser";
 import * as mysql from "mysql";
+
 let app = express();
 
 app.use(bodyParser.json());
