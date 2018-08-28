c35e63d174b8efc54b5d40f4c3fe900e192208ed
Zainan Victor Zhou <zzn@google.com>
2018-08-28 15:04:33 -0700

    Convert to typescript and format

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,22 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var http = require("http");
-
-
-var app = express();
-var bodyParser = require('body-parser');
+import * as express from "express";
+import * as http from "http";
+import * as bodyParser from "body-parser";
+import * as mysql from "mysql";
+let app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'pug');
 
-var server = http.createServer(app);
-var connect;
+let server = http.createServer(app);
+let connect, connection;
 
 if (process.env.OPEN_GOLINKS_GA_ID) {
-    console.log(`Setting Google Analytics with Tracking Id = `, process.env.OPEN_GOLINKS_GA_ID);
+  console.log(`Setting Google Analytics with Tracking Id = `, process.env.OPEN_GOLINKS_GA_ID);
   // Get the module
-  var expressGoogleAnalytics = require('express-google-analytics');
+  let expressGoogleAnalytics = require('express-google-analytics');
   // Insert your Google Analytics Id, Shoule be something like 'UA-12345678-9'
-  var analytics = expressGoogleAnalytics(process.env.OPEN_GOLINKS_GA_ID);
+  let analytics = expressGoogleAnalytics(process.env.OPEN_GOLINKS_GA_ID);
 
   //Add to express before your routes
   app.use(analytics);
@@ -26,8 +24,7 @@ if (process.env.OPEN_GOLINKS_GA_ID) {
 
 if (process.env.CLEARDB_DATABASE_URL) {
   console.log(`Using MySQL`);
-  var mysql = require('mysql');
-  var connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+  connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
   connection.connect();
 } else {
   console.log(`No MySQL specified`);
@@ -35,7 +32,7 @@ if (process.env.CLEARDB_DATABASE_URL) {
 }
 
 function upsertLinkAsync(linkname, dest, cb) {
-  var query = `
+  let query = `
 INSERT INTO golinks (linkname, dest, author)
 VALUES ('${linkname}', '${dest}', 'system')
 ON DUPLICATE KEY UPDATE
@@ -49,8 +46,9 @@ ON DUPLICATE KEY UPDATE
     cb(rows);
   })
 }
+
 function getLinkAsync(linkname, cb) {
-  var query = `SELECT linkname, dest, author from golinks WHERE linkname='${linkname}';`;
+  let query = `SELECT linkname, dest, author from golinks WHERE linkname='${linkname}';`;
 
   connection.query(query, function (err, rows, fields) {
     if (err) throw err;
@@ -63,9 +61,9 @@ function getLinkAsync(linkname, cb) {
   })
 }
 
-app.get('/:linkname([A-Za-z0-9-_]+)', function(req, res) {
-  var linkname = req.params.linkname;
-  getLinkAsync(linkname, function(dest) {
+app.get('/:linkname([A-Za-z0-9-_]+)', function (req, res) {
+  let linkname = req.params.linkname;
+  getLinkAsync(linkname, function (dest) {
     if (dest) {
       console.log('redirect to golink:', dest);
       res.redirect(dest);
@@ -76,26 +74,26 @@ app.get('/:linkname([A-Za-z0-9-_]+)', function(req, res) {
   });
 });
 
-app.get('/edit/:linkname([A-Za-z0-9-_]+)', function(req, res) {
-  console.log( `Editing`);
-  var linkname = req.params.linkname;
-  getLinkAsync(linkname, function(dest) {
+app.get('/edit/:linkname([A-Za-z0-9-_]+)', function (req, res) {
+  console.log(`Editing`);
+  let linkname = req.params.linkname;
+  getLinkAsync(linkname, function (dest) {
     console.log('Edit golink:', linkname, dest);
-    res.render('edit', { linkname: linkname, old_dest:dest})
+    res.render('edit', {linkname: linkname, old_dest: dest})
   });
 });
 
-app.post('/edit', function(req, res) {
-  console.log( `Posting`, req);
-  var linkname = req.body.linkname;
-  var dest = req.body.dest;
-  upsertLinkAsync(linkname, dest, function(rows) {
+app.post('/edit', function (req, res) {
+  console.log(`Posting`, req);
+  let linkname = req.body.linkname;
+  let dest = req.body.dest;
+  upsertLinkAsync(linkname, dest, function (rows) {
     console.log(`Done`);
     res.send('OK');
   });
 });
 
-var PORT = process.env.PORT || 3000;
+let PORT = process.env.PORT || 3000;
 console.log('Start listening on ', PORT);
 app.listen(PORT);
 
