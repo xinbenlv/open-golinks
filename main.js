7def747b106f6f668338bdff7c57ba0d252c951d
Zainan Victor Zhou <zzn@google.com>
2018-08-28 02:05:50 -0700

    Allow editing, with bootstrap

--- main.js
+++ main.js
@@ -24,34 +24,18 @@ if (process.env.OPEN_GOLINKS_GA_ID) {
   app.use(analytics);
 }
 
-var usingMySQL = false;
 if (process.env.CLEARDB_DATABASE_URL) {
-  usingMySQL = true;
   console.log(`Using MySQL`);
   var mysql = require('mysql');
   var connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
   connection.connect();
 } else {
-  console.log(`NOT Using MySQL`);
+  console.log(`No MySQL specified`);
+  process.exit(1);
 }
 
-app.get('/:linkname([A-Za-z0-9-_]+)', function(req, res) {
-  console.log(`try to direct to`, req.params.linkname, ' ...');
-  let golink = process.env['LINK_' + req.params.linkname];
-  if (golink) {
-    console.log('redirect to golink:', golink);
-    res.redirect(golink);
-  } else {
-    console.log('Not found', 'LINK_' + req.params.linkname);
-    res.status(404).send("No link for " + req.path);
-  }
-
-});
-
-if (usingMySQL) {
-
-  function upsertLinkAsync(linkname, dest, cb) {
-    var query = `
+function upsertLinkAsync(linkname, dest, cb) {
+  var query = `
 INSERT INTO golinks (linkname, dest, author)
 VALUES ('${linkname}', '${dest}', 'system')
 ON DUPLICATE KEY UPDATE
@@ -59,46 +43,57 @@ ON DUPLICATE KEY UPDATE
     author = 'system';
     `;
 
-    connection.query(query, function (err, rows, fields) {
-      if (err) throw err;
-      console.log('Result ', rows);
-      cb(rows);
-    })
-  }
-  function getLinkAsync(linkname, cb) {
-    var query = `SELECT linkname, dest, author from golinks WHERE linkname='${linkname}';`;
-
-    connection.query(query, function (err, rows, fields) {
-      if (err) throw err;
-      console.log('Result ', rows);
-      if (rows.length > 0) {
-        cb(rows[0].dest);
-      } else {
-        cb(null);
-      }
-    })
-  }
-
-  app.get('/edit/:linkname([A-Za-z0-9-_]+)', function(req, res) {
-    console.log( `Editing`);
-    var linkname = req.params.linkname;
-    let golink = process.env['LINK_' + linkname];
-    if (usingMySQL) getLinkAsync(linkname, function(dest) {
-      console.log('Edit golink:', linkname, dest);
-      res.render('edit', { linkname: linkname, old_dest:dest})
-    });
-  });
-
-  app.post('/edit', function(req, res) {
-    console.log( `Posting`, req);
-    var linkname = req.body.linkname;
-    var dest = req.body.dest;
-    upsertLinkAsync(linkname, dest, function(rows) {
-      console.log(`Done`);
-      res.send('OK');
-    });
-  });
+  connection.query(query, function (err, rows, fields) {
+    if (err) throw err;
+    console.log('Result ', rows);
+    cb(rows);
+  })
 }
+function getLinkAsync(linkname, cb) {
+  var query = `SELECT linkname, dest, author from golinks WHERE linkname='${linkname}';`;
+
+  connection.query(query, function (err, rows, fields) {
+    if (err) throw err;
+    console.log('Result ', rows);
+    if (rows.length > 0) {
+      cb(rows[0].dest);
+    } else {
+      cb(null);
+    }
+  })
+}
+
+app.get('/:linkname([A-Za-z0-9-_]+)', function(req, res) {
+  var linkname = req.params.linkname;
+  getLinkAsync(linkname, function(dest) {
+    if (dest) {
+      console.log('redirect to golink:', dest);
+      res.redirect(dest);
+    } else {
+      console.log('Not found', 'LINK_' + req.params.linkname);
+      res.redirect(`/edit/${linkname}`);
+    }
+  });
+});
+
+app.get('/edit/:linkname([A-Za-z0-9-_]+)', function(req, res) {
+  console.log( `Editing`);
+  var linkname = req.params.linkname;
+  getLinkAsync(linkname, function(dest) {
+    console.log('Edit golink:', linkname, dest);
+    res.render('edit', { linkname: linkname, old_dest:dest})
+  });
+});
+
+app.post('/edit', function(req, res) {
+  console.log( `Posting`, req);
+  var linkname = req.body.linkname;
+  var dest = req.body.dest;
+  upsertLinkAsync(linkname, dest, function(rows) {
+    console.log(`Done`);
+    res.send('OK');
+  });
+});
 
 var PORT = process.env.PORT || 3000;
 console.log('Start listening on ', PORT);
