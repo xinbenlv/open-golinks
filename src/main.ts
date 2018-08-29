4b5bded48fb9e4c8e285e2476b3aba3348ff731d
Zainan Victor Zhou <zzn@google.com>
2018-08-28 21:52:57 -0700

    Fix callback url

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,7 @@ console.assert(process.env.CLEARDB_DATABASE_URL, `$CLEARDB_DATABASE_URL is not s
 console.assert(process.env.AUTH0_DOMAIN, `$AUTH0_DOMAIN is not set`);
 console.assert(process.env.AUTH0_CLIENT_ID, `$AUTH0_CLIENT_ID is not set`);
 console.assert(process.env.AUTH0_CLIENT_SECRET, `AUTH0_CLIENT_SECRET is not set`);
+console.assert(, `AUTH0_CLIENT_SECRET is not set`);
 
 logger.debug(`Setting Google Analytics with Tracking Id = `, process.env.OPEN_GOLINKS_GA_ID);
 app.use(ua.middleware(process.env.OPEN_GOLINKS_GA_ID, {cookieName: '_ga'}));
@@ -32,7 +33,7 @@ var strategy = new Auth0Strategy({
       domain: process.env.AUTH0_DOMAIN,
       clientID: process.env.AUTH0_CLIENT_ID,
       clientSecret: process.env.AUTH0_CLIENT_SECRET, // Replace this with the client secret for your app
-      callbackURL: 'http://localhost:3000/callback'
+      callbackURL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
     },
     function (accessToken, refreshToken, extraParams, profile, done) {
       // accessToken is the token to call Auth0 API (not needed in the most cases)
