a30c511c34c43c344c421e08b69804037d4d3dc8
Zainan Victor Zhou <zzn@google.com>
2018-08-28 22:40:50 -0700

    Debug

--- src/main.ts
+++ src/main.ts
@@ -33,7 +33,6 @@ var strategy = new Auth0Strategy({
       clientID: process.env.AUTH0_CLIENT_ID,
       clientSecret: process.env.AUTH0_CLIENT_SECRET, // Replace this with the client secret for your app
       callbackURL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback',
-      state: true
     },
     function (accessToken, refreshToken, extraParams, profile, done) {
       // accessToken is the token to call Auth0 API (not needed in the most cases)
@@ -57,9 +56,9 @@ var sess = {
   saveUninitialized: true
 };
 
-if (app.get('env') === 'production') {
-  sess.cookie['secure'] = true; // serve secure cookies, requires https
-}
+// if (app.get('env') === 'production') {
+//   sess.cookie['secure'] = false; // serve secure cookies, requires https
+// }
 
 // app.js
 
